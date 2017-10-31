8b4b3726ebe0c55a4cfd6c18112c1abc646edaa6
ChathurikaAmarathunga <dck.amarathunga@gmail.com>
2017-10-31 22:35:24 +0530

    done changes required.

--- component/src/main/java/org/wso2/extension/siddhi/io/email/sink/EmailSink.java
+++ component/src/main/java/org/wso2/extension/siddhi/io/email/sink/EmailSink.java
@@ -382,14 +382,14 @@ public class EmailSink extends Sink {
             if (e.getCause() instanceof MailConnectException) {
                 if (e.getCause().getCause() instanceof ConnectException) {
                     throw new ConnectionUnavailableException("Error is encountered while connecting the smtp"
-                            + " server" +  e.getMessage(), e.getCause());
+                            + " server." +  e.getMessage(), e.getCause());
                 } else {
-                    throw new RuntimeException("Error is encountered while connecting to the smtp server" +
+                    throw new RuntimeException("Error is encountered while connecting to the smtp server." +
                             e.getMessage(), e.getCause());
                 }
             } else {
                 throw new RuntimeException("Error is encountered while connecting to the"
-                        + " the smtp server" + e.getMessage(), e);
+                        + " the smtp server." + e.getMessage(), e);
             }
         }
     }
